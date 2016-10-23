567e30d1cc310017b02aa45a934f03382c2ca8f4
Ant8 <antonem@o2.pl>
2016-10-23 12:45:16 +0200

    apka tylko w pionie zapytanie o token idzie do heroku

--- app/src/main/java/abm/ant8/brafhackaton/LoginActivity.kt
+++ app/src/main/java/abm/ant8/brafhackaton/LoginActivity.kt
@@ -2,20 +2,29 @@ package abm.ant8.brafhackaton
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import org.jetbrains.anko.*
+import java.net.URL
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        linearLayout {
+        verticalLayout {
             val playerNameView = editText {
                 hint = "Twój nick"
             }
             button("zaloguj") {
                 onClick {
-                    startActivity<MapsActivity>("token" to "123", "role" to "czlowiek", "name" to playerNameView.text.toString())
+                    doAsync {
+                        val response = URL("https://immense-wave-80129.herokuapp.com/api/token/${playerNameView.text.toString()}").readText()
+                        Log.d("dupa", response)
+                        uiThread {
+                            Log.d("dupa", "startuje!")
+                            startActivity<MapsActivity>("token" to "123", "role" to "czlowiek", "name" to playerNameView.text.toString())
+                        }
+                    }
                 }
             }
         }
